a488087966fa90ce47a7b1563c8ee702484444d8
boghtml <boghtml@gmail.com>
2025-12-09 20:14:40 +0200

    Add About and Contact pages with enhanced UI
    Introduced About and Contact views with rich content and styling. Updated HomeController to support new actions, improved Home/Index.cshtml with advanced filters, product cards, and interactive UI elements for a better user experience.

--- WorkoutShop/Controllers/HomeController.cs
+++ WorkoutShop/Controllers/HomeController.cs
@@ -45,6 +45,16 @@ namespace WorkoutShop.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
